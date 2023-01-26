ef72f52f25f877de455920b06d5e6ea6623e9647
Okonu <ianyakundi015@gmail.com>
2023-01-27 02:10:35 +0300

    commented img url

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,7 +8,7 @@ function LandingPage(){
             <p>This App Allows users to create and share albums of their photos. Sign Up or Login to get started!</p>
             <a href="/login">Login</a>
             <a href="/register">Register</a>
-        <im src={require('./images/image.png')} alt="App Demo" className="img-responsive"/>
+        {/* <im src={require('./images/image.png')} alt="App Demo" className="img-responsive"/> */}
         </div>
     );
 }
