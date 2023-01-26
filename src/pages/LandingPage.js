61c4a535daf174a5baf5103bda821e7fdebe8fec
Okonu <ianyakundi015@gmail.com>
2023-01-27 01:50:47 +0300

    landing page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './landingPage.css';
+
+function LandingPage(){
+    return(
+        <div>
+            <h1>Photo Sharing App</h1>
+            <p>This App Allows users to create and share albums of their photos. Sign Up or Login to get started!</p>
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+        <im src={require('./images/image.png')} alt="App Demo" className="img-responsive"/>
+        </div>
+    );
+}
+
+export default LandingPage;
