2aa4070d755572865d1120f79fe3c777cf009366
Okonu <ianyakundi015@gmail.com>
2023-01-27 18:17:59 +0300

    animate the opening and closing of the modal

--- src/pages/Modal.js
+++ src/pages/Modal.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import {motion} from "framer-motion"
 import './Modal.css';
 
+const backdrop = {
+    visible: {opacity: 1},
+    hidden: {opacity: 0}
+}
+
+const modal ={
+    hidden:{y:"-100vh", opacity: 0},
+    visible:{y:"200px", opacity: 1, transition: {delay:0.1}}
+}
 const Modal = ({ children, setIsModalOpen }) => {
     return (
-        <div className="modal-background">
-            <div className="modal-content">
-                {children}
-                <button onClick={()=>setIsModalOpen(false)}>Close</button>
-            </div>
-        </div>
+        <>
+        <motion.div className="backdrop"
+        variants={backdrop}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        onClick={()=>setIsModalOpen(false)}
+        />
+        <motion.div className="modal"
+        variants={modal}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        >
+            {children}</motion.div>
+        {/* // <div className="modal-background">
+        //     <div className="modal-content">
+        //         {children}
+        //         <button onClick={()=>setIsModalOpen(false)}>Close</button>
+        //     </div>
+        // </div> */}
+        </>
     );
 };
 
