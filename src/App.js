1e0609d9e3359993cd3eefead326977e511618f8
Okonu <ianyakundi015@gmail.com>
2023-01-27 19:20:19 +0300

    wrapping LandingPage with BrowserRouter component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
      <LandingPage/>
     </div>
+    </BrowserRouter>
   );
 }
 
