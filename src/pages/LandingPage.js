b11a17fdebb28f25d3d96f456e76c54bf6df0667
Okonu <ianyakundi015@gmail.com>
2023-01-27 17:41:44 +0300

    add a call-to-action button and some animations

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import './landingPage.css';
+import {motion, AnimatePresence } from "framer-motion"
+import Modal from './Modal';
 
 function LandingPage(){
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    useEffect(() => {
+        const timeout = setTimeout(()=> {
+            setIsModalOpen(true);
+        }, 3000);
+        return () => clearTimeout(timeout);
+    }, []);
     return(
         <div>
             <h1>Photo Sharing App</h1>
             <p>This App Allows users to create and share albums of their photos. Sign Up or Login to get started!</p>
-            <a href="/login">Login</a>
-            <a href="/register">Register</a>
+            <motion.a href="/login"
+            whileHover={{scale: 1.1}}
+            whileTap={{scale: 0.9}}
+            className="cta-button">Login</motion.a>
+            <motion.a href="/signup"
+            whileHover={{scale: 1.1}}
+            whileTap={{scale: 0.9}}
+            className="cta-button">Sign Up</motion.a>
         {/* <im src={require('./images/image.png')} alt="App Demo" className="img-responsive"/> */}
+        <AnimatePresence>
+            {isModalOpen && (<Modal setIsModalOpen={setIsModalOpen}>
+                <p>Sign Up Now to start creating albums and sharing your photos with the world.</p>
+                <button onClick={()=> setIsModalOpen(false)}>Close </button>
+                <a href="/signup" className="signup-button">Sign Up</a>
+            </Modal>)}
+        </AnimatePresence>
         </div>
     );
 }
