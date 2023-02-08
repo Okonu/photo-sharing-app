a265c315f81a16e381dd02ab2fb5bb8407ec9cc3
Okonu <ianyakundi015@gmail.com>
2023-02-08 20:03:34 +0300

    use useFirestore for createUser

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
-import { useFirebase } from 'react-redux-firebase';
+import { useFirebase, useFirestore } from 'react-redux-firebase';
+// import { useDispatch} from 'react-redux';
+import './authPage.css';
 
 function AuthPage() {
   const firebase = useFirebase();
+  const firestore = useFirestore();
+  // const dispatch = useDispatch();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
@@ -17,25 +21,35 @@ function AuthPage() {
   }
 
   function handleRegistration() {
-    firebase
-      .createUser({ email, password }, { email, password })
+    if (firebase)
+      {
+        firebase
+        .createUser({ email, password }, { email, password })
+      .then(({user}) => {firestore
+        .collection('users')
+        .doc(user.uid)
+        .set({email});
+      })
       .catch(err => setError(err.message));
+    }
   }
 
   return (
-    <div>
-      <h1>Authentication Page</h1>
-      {error && <p>{error}</p>}
-      <label>
+    <div className="auth-page">
+      <h1 className="auth-title">Login/SignUp to Access more features</h1>
+      {error && <p className="error-message">{error}</p>}
+      <label className="auth-label">
         Email:
         <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
       </label>
-      <label>
+      <label className="auth-label">
         Password:
         <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
       </label>
-      <button onClick={handleLogin}>Login</button>
-      <button onClick={handleRegistration}>Register</button>
+      <div className="auth-buttons">
+        <button className="auth-button" onClick={handleLogin}>Login</button>
+        <button className="auth-button" onClick={handleRegistration}>Register</button>
+      </div>
     </div>
   );
 }
