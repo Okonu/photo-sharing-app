f9536c66e05d4a91e9611705ac9a2fc18de0ad6f
Okonu <ianyakundi015@gmail.com>
2023-02-14 23:04:24 +0300

    import styling

--- src/pages/AlbumPage.js
+++ src/pages/AlbumPage.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect } from "react";
 import axios from "axios";
+import './albumPage.css';
 
 function AlbumPage({match}) {
     const [album, setAlbum ] = useState({});
